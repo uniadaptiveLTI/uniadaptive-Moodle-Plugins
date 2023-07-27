b0c3c34b279217134c987f4a59064c0aadd63bbc
David Jiménez <davidjimenezdaroqui@gmail.com>
2023-07-27 08:29:19 +0200

    change moodle version required

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_uniadaptive';
-$plugin->version = 2022010100;
-$plugin->requires = 2021051700;
+$plugin->version = 2023072700;
+$plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'v1.0';
