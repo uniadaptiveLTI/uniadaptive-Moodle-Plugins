f1cac9deb1390f1a06cd0c8f54cd9559e02dfdef
BAOOBAP <davidjimenezdaroqui@gmail.com>
2024-02-20 12:03:39 +0100

    version change

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_uniadaptive';
-$plugin->version = 2023092005;
+$plugin->version = 2024022005;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v1.0';
+$plugin->release = 'v1.9';
